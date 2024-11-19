a2391b4c1eed6dcd2d841e8f3e5c2bf6b5acb1f2
Vitalika <vitamin@MacBook-Air-Vitalika.local>
2024-11-19 15:59:00 +0300

    add .new branch_with_allure

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,22 +1,36 @@
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.TextBoxPage;
+import static io.qameta.allure.Allure.step;
 
 public class TextBoxTests extends TestBase {
 
     TextBoxPage textBoxPage = new TextBoxPage();
 
+@Tag("Smoke")
+@Tag("Заполнение формы регистрации")
 @Test
     void textBoxPageTest() {
+    step("Открыть страницу с формой", () -> {
+        textBoxPage.openTextPage();
+    });
+
+    step("Заполнить поля", () -> {
     textBoxPage
-            .openTextPage()
             .setFullName("Mr.Bin")
             .setUserEmail("misterbin@internet.com")
             .setCurrentAddress("London,Oxford street")
-            .setPermanentAddress("New York,USA")
-            .submit();
+            .setPermanentAddress("New York,USA");
+    });
 
+    step("Отправить форму", () -> {
+        textBoxPage.submit();
+    });
+
+    step("Проверить отображение полей", () -> {
     textBoxPage.
             checkResult("Mr.Bin", "misterbin@internet.com", "London,Oxford street","New York,USA" );
+});
     }
 
 }
